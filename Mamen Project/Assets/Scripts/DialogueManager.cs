f86e8da6b2aba4e2df6a0c71005ca94f0a8242db
Pyrozen <william.oky@binus.ac.id>
2019-07-16 17:39:11 +0700

    Updating Opening Sequence

---
+++ Mamen Project/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject[] dialogueLines;
+    private int i=0;
+    private GameObject currentLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLine = dialogueLines[0];
+        currentLine.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentLine.GetComponent<Typewriter>().done && i<dialogueLines.Length)
+        {
+            i++;
+            Thread.Sleep(currentLine.GetComponent<Typewriter>().delay);
+            currentLine.SetActive(false);
+            currentLine = dialogueLines[i];
+            currentLine.SetActive(true);
+        }
+    }
+}
---
+++ Mamen Project/Assets/Scripts/Typewriter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Typewriter : MonoBehaviour
+{
+    public TextMeshProUGUI displayText;
+    public TextMeshProUGUI inputText;
+
+    private string outputString = "";
+    private int i = -1;
+    public bool done;
+    public int delay;
+
+    void Update()
+    {
+        if(!done)
+        {
+            displayText.text = TypeWrite(inputText.text);
+        }
+    }
+    private string TypeWrite(string input)
+    {
+        i++;
+        char[] character = input.ToCharArray();
+        outputString = outputString + character[i].ToString();
+        if(i==character.Length-1)
+        {
+            done = true;
+        }
+        return outputString;
+    }
+}
